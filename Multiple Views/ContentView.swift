11e6653eeb0c0983058801d467a1bfe83ebedc49
tyler0611 <berlity0611@gmail.com>
2024-09-27 08:57:18 -0500

    Initial Commit

---
+++ Multiple Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Multiple Views
+//
+//  Created by Tyler Berlin on 9/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Multiple Views/Multiple_ViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiple_ViewsApp.swift
+//  Multiple Views
+//
+//  Created by Tyler Berlin on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Multiple_ViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
