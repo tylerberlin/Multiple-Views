ac6e7ae5b7971ae3776c0d8b387983e2fa1b8c42
tyler0611 <berlity0611@gmail.com>
2024-10-30 10:21:50 -0500

    Multiple Views

---
+++ Multiple Views/SwiftUIView01.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView01.swift
+//  Multiple Views
+//
+//  Created by Tyler Berlin on 9/27/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView01: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView01()
+}
---
+++ Multiple Views/SwiftUIView03.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView03.swift
+//  Multiple Views
+//
+//  Created by Tyler Berlin on 9/27/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView03: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView03()
+}
